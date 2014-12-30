c1564389ab792bd7583bb7faf65ff38aff97c1cb
Mitchell Wills <mwills@wpi.edu>
2014-12-30 14:26:23 -0500

    Added some comments to the HTTP reply methods

--- include/async_web_server_cpp/http_reply.hpp
+++ include/async_web_server_cpp/http_reply.hpp
@@ -41,35 +41,60 @@ struct HttpReply
 
   static std::vector<boost::asio::const_buffer> to_buffers(const std::vector<HttpHeader> &headers);
 
+  /**
+   * Create a request handler that sends a stock reply based on the stats code
+   */
   static HttpServerRequestHandler stock_reply(status_type status);
 
+  /**
+   * Create a request handler that sends the contents of a file
+   * NOTE: the file is only loaded once when the request handler is created
+   */
   static HttpServerRequestHandler from_file(HttpReply::status_type status,
       const std::string& content_type,
       const std::string& filename,
       const std::vector<HttpHeader>& additional_headers = std::vector<HttpHeader>());
 
+  /**
+   * Create a request handler that sends a static response
+   */
   static HttpServerRequestHandler static_reply(status_type status,
       const std::string& content_type,
       const std::string& content,
       const std::vector<HttpHeader>& additional_headers = std::vector<HttpHeader>());
 
+  /**
+   * Create a builder to create and send reply headers
+   */
   static ReplyBuilder builder(status_type status);
 };
 
 /**
- * Object to build and send a reply
+ * Object to build and send reply headers
  */
 class ReplyBuilder
 {
 public:
   ReplyBuilder(HttpReply::status_type status);
 
+  /**
+   * Add a header to the reply
+   */
   ReplyBuilder &header(const std::string &name, const std::string &value);
 
+  /**
+   * Add a header to the reply
+   */
   ReplyBuilder &header(const HttpHeader &header);
 
+  /**
+   * Add a group of headers to the reply
+   */
   ReplyBuilder &headers(const std::vector<HttpHeader> &headers);
 
+  /**
+   * Send the headers over the connection
+   */
   void write(HttpConnectionPtr connection);
 
 private:
@@ -79,7 +104,7 @@ private:
 
 
 /**
- *  Request Handler that serves a a predefined response
+ *  Request Handler that serves a predefined response
  */
 class StaticHttpRequestHandler
 {
